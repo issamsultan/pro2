a6c869c611f771e0f04f29628f31e88cba3a0642
Issam Sultan <issamsultan@gmail.com>
2014-02-24 16:32:41 +0000

    removed tag and phototag model which are no longer needed

--- stockoverflow/app/models/photo.rb
+++ stockoverflow/app/models/photo.rb
@@ -1,12 +1,11 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :album_id, :description, :flag, :image, :question, :rating, :title, :answer_ids, :photo_tags_attributes, :tag_ids, :remote_image_url, 
+  attr_accessible :album_id, :description, :flag, :image, :question, :rating, :title, :answer_ids, :remote_image_url, 
 
   mount_uploader :image, ImageUploader
 
   belongs_to :album
   has_many :answers
-  has_many :photo_tags
-  has_many :tags, through: :photo_tags
+  
 
   #makes the photo model voteable
   acts_as_voteable
