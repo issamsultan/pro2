42946a2fba63dee2b63a8b3c047232fd8c6533d4
Max Norton <maxnorton.nz@gmail.com>
2014-02-26 15:17:47 +0000

    ability for current users to create and edit their own photos, albums and answers

--- stockoverflow/app/models/ability.rb
+++ stockoverflow/app/models/ability.rb
@@ -7,11 +7,31 @@ class Ability
     if user.role?(:admin)
       can :manage, :all
     elsif user.role?(:registered) 
-      can :create, Photo
       can :read, :all
+
+      can :create, Photo
       can :edit, Photo do |photo|
         photo.try(:user_id) == user.id 
       end
+      can :destroy, Photo do |photo|
+        photo.try(:user_id) == user.id 
+      end
+
+      can :create, Answer
+      can :edit, Answer do |answer|
+        answer.try(:user_id) == user.id 
+      end
+      can :destroy, Answer do |answer|
+        answer.try(:user_id) == user.id 
+      end
+      can :create, Album
+      can :edit, Album do |album|
+        album.try(:user_id) == user.id 
+      end
+      can :destroy, Album do |album|
+        album.try(:user_id) == user.id 
+      end
+
     else 
       can :read, Photo
       can :create, User
