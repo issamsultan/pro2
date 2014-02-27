bff2b8116f9e1954d7bfa63ca8ca3551037f45c0
Issam Sultan <issamsultan@gmail.com>
2014-02-27 11:11:28 +0000

    new migration where added name to user and made changes to header to show name of user when logged in

--- stockoverflow/db/schema.rb
+++ stockoverflow/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140225192745) do
+ActiveRecord::Schema.define(:version => 20140227110356) do
 
   create_table "albums", :force => true do |t|
     t.string   "title"
@@ -29,20 +29,6 @@ ActiveRecord::Schema.define(:version => 20140225192745) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  create_table "comments", :force => true do |t|
-    t.string   "title",            :limit => 50, :default => ""
-    t.text     "comment"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.integer  "user_id"
-    t.datetime "created_at",                                     :null => false
-    t.datetime "updated_at",                                     :null => false
-  end
-
-  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
-  add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
-  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "photos", :force => true do |t|
     t.string   "image"
@@ -102,6 +88,7 @@ ActiveRecord::Schema.define(:version => 20140225192745) do
     t.datetime "updated_at",                             :null => false
     t.string   "image"
     t.string   "role"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
